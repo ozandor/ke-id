2f0593f958d2460cbb394bbf6ac1a6ceb875f88d
Ozan Doruk Yavuz <ozandorukyavuz@hotmail.com>
2025-06-22 06:36:11 +0300

    authOptions debug, Dockerization of the app,
    authOptions carried to the lib, (as auth.ts) for modern impl. and debug.
    
    Created Dockerfile and docker-compose.yml.
    (Not given version. To run with modern syntax, use `docker compose up --build` to build docker. )
    
    In Dockerfile, passed public environment args during the build to fix the logout URL. (for Auth0 config)

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# install dependencies
+FROM node:22-alpine AS deps
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+
+# build app
+FROM node:22-alpine AS builder
+WORKDIR /app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+# WE HAVE TO DECLARE THESE ARGUMENTS FOR AUTH0 TO WORK PROPERLY!!
+ARG NEXT_PUBLIC_AUTH0_DOMAIN
+ARG NEXT_PUBLIC_AUTH0_CLIENT_ID
+RUN npm run build
+
+# production image created
+FROM node:22-alpine AS runner
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+# copied built assets from builder
+COPY --from=builder /app/.next/standalone ./
+COPY --from=builder /app/.next/static ./.next/static
+
+EXPOSE 3000
+
+CMD ["node", "server.js"] 
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,5 @@
 import NextAuth from "next-auth"
-import type { NextAuthOptions } from "next-auth"
-import Auth0Provider from "next-auth/providers/auth0"
-
-export const authOptions: NextAuthOptions = {
-  // Auth0 providers
-  providers: [
-    Auth0Provider({
-      clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-      issuer: process.env.NEXT_PUBLIC_AUTH0_DOMAIN!,
-    }),
-    // We can add more OAuth here like Google, Github, Microsoft etc.
-  ],
-
-  session: { strategy: "jwt" },
-  secret: process.env.NEXTAUTH_SECRET!,
-
-  callbacks: {
-    // JWT callback
-    async jwt({ token, account }) {
-      // If this is a sign in, add the user's role from the access token to the JWT.
-      if (account?.id_token) {
-        // This namespace matching Auth0's namespace
-        const namespace = "https://next-auth-example.com"
-        const decodedToken = JSON.parse(
-          Buffer.from(account.id_token.split(".")[1], "base64").toString()
-        )
-        const roles = decodedToken[`${namespace}/roles`] as string[] | undefined
-        token.role = roles?.[0] // Assigns the role to our JWT token
-      }
-      return token
-    },
-
-    // In each session check, we add the role to the user's session object.
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.role = token.role
-      }
-      return session
-    },
-  },
-}
+import { authOptions } from "@/lib/auth"
 
 // NextAuth handler
 const handler = NextAuth(authOptions)
